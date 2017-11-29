7ffd123ea675fa59a4734b442e5dbf8e3bbebeb3
Gabriel Ramirez Ramirez <gabrira09@gmail.com>
2017-11-28 22:32:57 -0600

    -- librería para MongoDB en Netbeans. -- Ventana de Interfaces.

--- TareaInvestigacion-MONGODB/src/tareainvestigacion/mongodb/TareaInvestigacionMONGODB.java
+++ TareaInvestigacion-MONGODB/src/tareainvestigacion/mongodb/TareaInvestigacionMONGODB.java
@@ -5,16 +5,34 @@
  */
 package tareainvestigacion.mongodb;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Marisol CH
  * prueba
  */
 public class TareaInvestigacionMONGODB {
-
+    DB db;
+    DBCollection tabla;
     /**
      * @param args the command line arguments
      */
+    public void conexion(){
+        try {
+            Mongo mongo  = new Mongo("localhost", 27017);
+            db = mongo.getDB("practInvestigacion");
+            tabla = db.getCollection("producto");
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(TareaInvestigacionMONGODB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
+    }
     public static void main(String[] args) {
         // TODO code application logic here
     }
