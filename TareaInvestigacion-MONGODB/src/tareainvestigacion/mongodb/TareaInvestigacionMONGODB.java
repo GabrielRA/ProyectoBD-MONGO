103625ac2700f8d70fc795541d2a22c1e9e00c58
Gabriel Ramirez Ramirez <gabrira09@gmail.com>
2017-11-29 07:43:20 -0600

    -- Conexión funcionando.

--- TareaInvestigacion-MONGODB/src/tareainvestigacion/mongodb/TareaInvestigacionMONGODB.java
+++ TareaInvestigacion-MONGODB/src/tareainvestigacion/mongodb/TareaInvestigacionMONGODB.java
@@ -18,21 +18,9 @@ import java.util.logging.Logger;
  * prueba
  */
 public class TareaInvestigacionMONGODB {
-    DB db;
-    DBCollection tabla;
     /**
      * @param args the command line arguments
-     */
-    public void conexion(){
-        try {
-            Mongo mongo  = new Mongo("localhost", 27017);
-            db = mongo.getDB("practInvestigacion");
-            tabla = db.getCollection("producto");
-        } catch (UnknownHostException ex) {
-            Logger.getLogger(TareaInvestigacionMONGODB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-}
-    }
+    */
     public static void main(String[] args) {
         // TODO code application logic here
     }
